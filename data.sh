c473c546fe5bf4d38fca41dd908459e815021cc6
shivam-s29 <153683472+shivam-s29@users.noreply.github.com>
2024-05-30 12:01:45 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-sudo yum update -y
 sudo yum install -y python3 python3-pip git
-sudo git clone https://github.com/shivam-s29/Agri.git
-cd /home/ec2-user/Agri
+sudo git clone https://github.com/shivam-s29/fish.git
+cd /fish
+sudo yum install -y python3 python3-pip
 pip3 install -r requirements.txt
 screen -dmS Agri python3 app.py
