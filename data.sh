a8ffdf1f7b16c3d591720c35b6db8077886ae8fd
shivam-s29 <153683472+shivam-s29@users.noreply.github.com>
2024-05-30 12:19:39 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+# Install Git
 sudo yum install git -y
-sudo git clone https://github.com/shivam-s29/car-prediction.git
-cd /
+
+# Clone the GitHub repository
+git clone https://github.com/shivam-s29/car-prediction.git
+
+# Change to the cloned repository directory
 cd car-prediction/
+
+# Install Python 3 and Pip
 sudo yum install python3 python3-pip -y
+
+# Install Python dependencies
 pip3 install -r requirements.txt
-screen -m -d python3 app.py
+
+# Run the application in a detached screen session
+screen -dmS car_prediction_app python3 app.py
